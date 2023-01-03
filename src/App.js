9ce3908ee7bfe44161937e1d4776f62d1b347c1c
michalskimateusz <michalskimateusz@outlook.com>
2023-01-03 13:58:45 +0100

    setting up and fetching data from mock server with json-server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import "./App.scss";
 import Layout from "./components/ui/Layout";
 import Card from "./components/Card/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData().then(() => setIsLoading(false));
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/tours");
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (isLoading) return <h1>Loading</h1>;
+
   return (
     <Layout>
       <Card />
